e5e0938845ae515edfda140e3cc8501c350dcb5e
Jonathan Islas <islas.27@gmail.com>
2017-03-18 16:55:05 -0700

    Added Favicon and changed categories display color

--- html.js
+++ html.js
@@ -17,13 +17,20 @@ module.exports = React.createClass({
         if (process.env.NODE_ENV === 'production') {
             css = <style dangerouslySetInnerHTML={ {    __html: require('!raw!./public/styles.css')} } />
         }
-
         return (
             <html lang="en">
             <head>
               <meta charSet="utf-8" />
               <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
               <meta name="viewport" content="width=device-width, initial-scale=1.0 maximum-scale=5.0" />
+              <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png"/>
+              <link rel="icon" type="image/png" href="/favicon/favicon-32x32.png" sizes="32x32"/>
+              <link rel="icon" type="image/png" href="/favicon/favicon-16x16.png" sizes="16x16"/>
+              <link rel="manifest" href="/favicon/manifest.json"/>
+              <link rel="mask-icon" href="/favicon/safari-pinned-tab.svg" color="#49a010"/>
+              <link rel="shortcut icon" href="/favicon/favicon.ico"/>
+              <meta name="msapplication-config" content="/favicon/browserconfig.xml"/>
+              <meta name="theme-color" content="#ffffff"/>
               { title.toComponent() }
               { font }
               { css }
@@ -34,5 +41,5 @@ module.exports = React.createClass({
             </body>
             </html>
         )
-    },
+    }
 })
